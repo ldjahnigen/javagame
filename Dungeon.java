f9ab83a6477adf0224625cc1aec11a7e48446e53
louisj <ldjahnigen@gmail.com>
2023-03-21 14:35:33 -0400

    working hall placement, complete map builder

--- Dungeon.java
+++ Dungeon.java
@@ -2,6 +2,7 @@ import java.util.ArrayList;
 
 public class Dungeon {
   char[][] map;
+  int[] center = {0, 0};
   
   public Dungeon(char[][] map_) {
     map = map_;
--- MapGenerator.java
+++ MapGenerator.java
@@ -1,5 +1,6 @@
 import java.util.Random;
 import java.util.ArrayList;
+import java.lang.Math;
 
 public class MapGenerator {
   String file;
@@ -19,11 +20,67 @@ public class MapGenerator {
     System.out.println(text);
   }
 
-  public char[][] jahnigen(int x1, int y1, int x2, int y2, char[][] map) {
-    float m = (y2 - y1) / (x2 - x1);
-    System.out.println(m);
-    return map;
+  public int[][] bresenham(int startx, int starty, int endx, int endy) {
+    int dx = Math.abs(endx - startx);
+    int dy = Math.abs(endy - starty);
+    int sx = startx < endx ? 1 : -1;
+    int sy = starty < endy ? 1 : -1;
+    int err = dx - dy;
+    int x = startx;
+    int y = starty;
+    int[][] coordinates = new int[Math.max(dx, dy) + 1][2];
+    int i = 0;
+    while (true) {
+        coordinates[i][0] = x;
+        coordinates[i][1] = y;
+        i++;
+        if (x == endx && y == endy) {
+            break;
+        }
+        int e2 = 2 * err;
+        if (e2 > -dy) {
+            err = err - dy;
+            x = x + sx;
+        }
+        if (e2 < dx) {
+            err = err + dx;
+            y = y + sy;
+        }
+    }
+    return coordinates;
   }
+
+  public void connectDoors(int startx, int starty, int endx, int endy, char[][] map) {
+    Random rand = new Random();
+    int choice = rand.nextInt(0, 2);
+    int cornerx = 0;
+    int cornery = 0;
+
+    if (choice == 0) {
+      cornerx = endx;
+      cornery = starty;
+    } else if (choice == 1) {
+      cornerx = startx; 
+      cornery = endy;
+    }
+    int[][] points1 = bresenham(startx, starty, cornerx, cornery);
+    int[][] points2 = bresenham(cornerx, cornery, endx, endy);
+    int x = 0;
+    int y = 0;
+    for (int i = 0; i < points1.length; i++) {
+      x = points1[i][0];
+      y = points1[i][1];
+
+      map[y][x] = '.';
+    }
+    for (int i = 0; i < points2.length; i++) {
+      x = points2[i][0];
+      y = points2[i][1];
+
+      map[y][x] = '.';
+    }
+  }
+
 
   public void generateMap(int x, int y) {
     char[][] map = new char[y][x];
@@ -47,9 +104,9 @@ public class MapGenerator {
 
     // begin dungeon placement loop
     int dungeon_count = 20;
-    int placed_dungeons = 0;
-    for (int i = 0; i < dungeon_count; i++) {
+    ArrayList<Dungeon> dungeons = new ArrayList<Dungeon>();
 
+    for (int i = 0; i < dungeon_count; i++) {
       // define dimensions
       Random rand = new Random();
       int width = rand.nextInt(5, 25);
@@ -76,6 +133,7 @@ public class MapGenerator {
 
       // create dungeon object to hold pattern
       Dungeon dungeon = new Dungeon(new char[height][width]); 
+      dungeon.center = new int[] {xcoord + (width / 2), ycoord + (height / 2)};
 
       String nw = Character.toString(map[ycoord][xcoord]);
       String ne = Character.toString(map[ycoord + height - 1][xcoord + width - 1]);
@@ -89,41 +147,16 @@ public class MapGenerator {
         }
       }
       
-      // outline with '#' and place doors
+      // outline with '#'
       for (int ii = 0; ii < dungeon.map[0].length; ii++) {
-        dungeon.map[0][ii] = 'H';
-        dungeon.map[dungeon.map.length - 1][ii] = 'H';
+        dungeon.map[0][ii] = ' ';
+        dungeon.map[dungeon.map.length - 1][ii] = ' ';
       }
       for (int ii = 0; ii < dungeon.map.length; ii++) {
-        dungeon.map[ii][0] = 'H';
-        dungeon.map[ii][dungeon.map[0].length - 1] = 'H';
+        dungeon.map[ii][0] = ' ';
+        dungeon.map[ii][dungeon.map[0].length - 1] = ' ';
       }
      
-      int side = 1;
-      int door_spot = 0;
-      switch (side) {
-        case (0):
-          door_spot = rand.nextInt(1, dungeon.map[0].length - 2); 
-          dungeon.map[0][door_spot] = '>';
-          break;
-
-        case (1):
-          door_spot = rand.nextInt(1, dungeon.map.length - 2); 
-          dungeon.map[door_spot][0] = '>';
-          break;
-
-        case (2):
-          door_spot = rand.nextInt(1, dungeon.map[0].length - 2); 
-          dungeon.map[0][door_spot] = '>';
-          break;
-
-        case (3):
-          door_spot = rand.nextInt(1, dungeon.map[0].length - 2); 
-          dungeon.map[0][door_spot] = '>';
-          break;
-      }
-
-
       // check for overlaps at the corners
       if (nw.equals(".") || nw.equals("H")) {
         continue;
@@ -150,30 +183,49 @@ public class MapGenerator {
         ycoordi++;
         xcoordi = xcoord; 
       }
-      placed_dungeons++;
+      dungeons.add(dungeon);
     }
-   
-    // begin hall placement loop
-    for (int i = 0; i < placed_dungeons; i++) {
-      // select which tiles to use as doors
-      Random rand = new Random();
-      int side = rand.nextInt(0, 3);
-      switch (side) {
-        case (0):
-           
-          break;
+  
+    // place the halls
+    for (int i = 0; i < dungeons.size(); i++) {
+      try {
+      connectDoors(
+          dungeons.get(i).center[0],
+          dungeons.get(i).center[1],
+          dungeons.get(i + 1).center[0],  
+          dungeons.get(i + 1).center[1],
+          map
+          );
+      } catch (IndexOutOfBoundsException e) {
+      connectDoors(
+          dungeons.get(i).center[0],
+          dungeons.get(i).center[1],
+          dungeons.get(0).center[0],  
+          dungeons.get(0).center[1],
+          map
+          );
+      }
+    }
 
-        case (1):
-
-          break;
-
-        case (2):
-
-          break;
-
-        case (3):
-
-          break;
+    // place 'H's to indicate walls
+    char[] snapshot = new char[8];
+    for (int i = 0; i < map.length; i++) {
+      for (int j = 0; j < map[0].length; j++) {
+        if (Character.toString(map[i][j]).equals(" ")) {
+          snapshot[0] = map[i - 1][j];
+          snapshot[1] = map[i + 1][j];
+          snapshot[2] = map[i][j - 1];
+          snapshot[3] = map[i][j + 1];
+          snapshot[4] = map[i - 1][j - 1];
+          snapshot[5] = map[i + 1][j + 1];
+          snapshot[6] = map[i - 1][j + 1];
+          snapshot[7] = map[i + 1][j - 1];
+          for (char c : snapshot) {
+            if (Character.toString(c).equals(".")) {
+              map[i][j] = 'H';
+            }
+          }
+        }
       }
     }
 
